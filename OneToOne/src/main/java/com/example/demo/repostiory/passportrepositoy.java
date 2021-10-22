f7046c04dc54a4ff17e05823cc5a0b06fd9f301a
carynd <caryntech@gmail.com>
2021-10-22 13:49:29 +0530

    1

--- OneToOne/src/main/java/com/example/demo/repostiory/passportrepositoy.java
+++ OneToOne/src/main/java/com/example/demo/repostiory/passportrepositoy.java
@@ -2,7 +2,8 @@ package com.example.demo.repostiory;
 
 import com.example.demo.entity.passport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface passportrepositoy  extends JpaRepository<passport, Long> {
 }
--- OneToOne/src/main/java/com/example/demo/repostiory/studentrepository.java
+++ OneToOne/src/main/java/com/example/demo/repostiory/studentrepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repostiory;
 
 import com.example.demo.entity.student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface studentrepository   extends JpaRepository<student, Long> {
 }
